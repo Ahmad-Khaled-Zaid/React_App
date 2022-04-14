5f251cd0b3768d827964a47ff723232bf34fab5d
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2022-04-14 09:13:32 +0300

    add Modal

--- reactlibrary/src/App.js
+++ reactlibrary/src/App.js
@@ -1,12 +1,47 @@
 import React from "react";
 import Parent from "./components/Parent";
 import "bootstrap/dist/css/bootstrap.min.css";
+import DataModal from "./components/Modal";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      modalTitle: "",
+      modalText: "",
+      modalImage: "",
+    };
+  }
+
+  handleClose = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
+
+  modalInfo = (modalTitle, modalImage, modalText) => {
+    this.setState({
+      modalTitle: modalTitle,
+      modalText: modalText,
+      modalImage: modalImage,
+    });
+  };
+
   render() {
     return (
       <div>
-        <Parent />
+        <Parent
+          handleCloseParent={this.handleClose}
+          modalInfo={this.modalInfo}
+        />
+        <DataModal
+          show={this.state.show}
+          close={this.handleClose}
+          title={this.state.modalTitle}
+          text={this.state.modalText}
+          image={this.state.modalImage}
+        />
       </div>
     );
   }
--- reactlibrary/src/components/Child.js
+++ reactlibrary/src/components/Child.js
@@ -3,30 +3,33 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 class Child extends React.Component {
-  state = {
-    votes: 0,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: 0,
+    };
+  }
 
   addVoted = () => {
     this.setState({
       votes: this.state.votes + 1,
     });
+    this.props.handleCloseChild();
+    this.props.modalInfo(this.props.title, this.props.image, this.props.text);
   };
   render() {
     return (
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={this.props.image} />
-        <Card.Body>
-          <Card.Title>{this.props.title}</Card.Title>
-          <Card.Text>{this.props.text}</Card.Text>
-          <Card.Text>{this.props.vote}</Card.Text>
-          <Card.Text>{this.state.votes}</Card.Text>
-
-          <Button  onClick={this.addVoted}>
-            Pick this Horned Beast
-          </Button>
-        </Card.Body>
-      </Card>
+      <div>
+        <Card style={{ width: "18rem" }} class="Card">
+          <Card.Img variant="top" src={this.props.image} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>{this.props.text}</Card.Text>
+            <Card.Text>{this.state.votes} </Card.Text>
+            <Button onClick={this.addVoted}>Pick this Horned Beast</Button>
+          </Card.Body>
+        </Card>
+      </div>
     );
   }
 }
